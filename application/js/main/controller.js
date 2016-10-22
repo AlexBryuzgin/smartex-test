5b22e7c27d9ba5e3195dc6ac26ac24e5c2b327a2
Александр Брюзгин < >
2016-10-22 15:10:37 +0300

    bonus task for bookmark done

--- application/js/main/controller.js
+++ application/js/main/controller.js
@@ -7,27 +7,38 @@
 	function MainPageController($scope, restService, localStorageService){
 		var vm = this;
 
+		//get data from API
 		vm.users = [];
 		restService.getData()
 		.$promise
 		.then(function(response){
 			vm.users = response;
 			console.log(vm.users);
-		})
+		});
+		//show detalisation
 		vm.user = {};
 		vm.showflag = false;
 		vm.goto = function(id){
 			vm.showflag = true;
 			localStorageService.set('userId', id);
-			vm.user = vm.users[id - 1];
-			console.log(vm.user)
-		}
+			if(localStorageService.get('userData') == undefined || localStorageService.get('userData') == null )
+				localStorageService.set('userData',  vm.users[id - 1]);
+			vm.user = localStorageService.get('userData');
+			console.log(vm.user);
+		};
+		//close pop-up detalisation window
 		vm.closePopup = function(){
 			vm.showflag = false;
 			localStorageService.clearAll();
-		}
-		vm.prevent = function(event){
-			event.preventDefault();
-		}
+		};
+		vm.localId = localStorageService.get('userId');
+		$scope.$watch(function(){
+			return vm.localId
+		}, function(){
+			if(vm.localId != undefined){
+				console.log(vm.localId);
+				vm.goto(vm.localId);
+			};
+		});
 	}
 }());
